2597a261ea8de05fcef32f51bd95cbfd9ff5106d
Casey N <caseyn@Caseys-MBP.fios-router.home>
2020-11-09 11:35:45 -0600

    Add github pages config

--- src/api/products.js
+++ src/api/products.js
@@ -7,23 +7,3 @@ export const products =[
         
 ]
 
-export const productss = [
-    {
-      id:1,
-    emoji: '🍦',
-    name: 'ice cream',
-    price: 5
-  },
-  {
-    id:2,
-    emoji: '🍩',
-    name: 'donuts',
-    price: 2.5,
-  },
-  {
-    id:3,
-    emoji: '🍉',
-    name: 'watermelon',
-    price: 4
-  }
-]
