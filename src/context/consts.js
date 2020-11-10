c7a77ed6cf716f11755968aa6d933b268ae6de1e
kc2040 <67342640+kc2040@users.noreply.github.com>
2020-11-09 18:47:30 -0600

    Update consts.js

--- src/context/consts.js
+++ src/context/consts.js
@@ -1,6 +1,6 @@
-//export const ASSETS_FOLDER = 'https://raw.githubusercontent.com/kc2040/rtw/main/public/assets/';
-export const ASSETS_FOLDER = 'assets/';
+export const ASSETS_FOLDER = 'https://raw.githubusercontent.com/kc2040/rtw/main/public/assets/';
+//export const ASSETS_FOLDER = 'assets/';
 
-//export const PUBLIC_FOLDER = 'https://raw.githubusercontent.com/kc2040/rtw/main/public/';
-export const PUBLIC_FOLDER = '';
+export const PUBLIC_FOLDER = 'https://raw.githubusercontent.com/kc2040/rtw/main/public/';
+//export const PUBLIC_FOLDER = '';
 
